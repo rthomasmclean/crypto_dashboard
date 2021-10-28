86ca714a75e2cbe19398a559aad44bbad6c3a0e9
Reed Thomas-McLean <62526412+rthomasmclean@users.noreply.github.com>
2021-10-28 11:27:36 -0700

    Insert edit

--- arb_table.py
+++ arb_table.py
@@ -6,22 +6,22 @@ class table:
         self.dict = {}
         
     def update_table(self, name, exchange, borrow, deposit, tvl):
-        data = [exchange, borrow, deposit, tvl]
-        cols = ["Exchange", "Cost to Borrow", "Yield", "TVL"]
-        entry = pd.DataFrame([data], columns=cols)
-        # Append data when the seeing exchange for first time in a non-empty key
+        entry = {"Exchange": exchange, "Cost to Borrow": borrow, 
+                 "Yield": deposit, "TVL": tvl}
+        # non-empty key
         if (name in self.dict.keys() and 
             self.dict[name][self.dict[name]["Exchange"] == exchange].empty):
-            entry = pd.DataFrame([data], columns=cols)
             self.dict[name] = self.dict[name].append(entry, ignore_index=True)
         # Replace data when exchange is already in key
         elif (name in self.dict.keys() and 
-            (self.dict[name][self.dict[name]["Exchange"] == exchange].empty == False)):
-            index = self.dict[name].index[self.dict[name]["Exchange"] == exchange]
-            self.dict[name].loc[index] = data
+             (self.dict[name][self.dict[name]["Exchange"] == exchange] \
+             .empty == False)):
+            index = self.dict[name] \
+                        .index[self.dict[name]["Exchange"] == exchange]
+            self.dict[name].loc[index] = [exchange, borrow, deposit, tvl]
         # First row in previously empty DataFrame
         else:
-            self.dict[name] = entry
+            self.dict[name] = pd.DataFrame(entry, index=[0])
     
     def print_table(self):
         print(self.dict)
@@ -31,8 +31,10 @@ class table:
         spreads = {}
         for key in self.dict.keys():
             max_yield = self.dict[key]["Yield"].max()
-            max_exchange = self.dict[key]["Exchange"][self.dict[key]["Yield"] == max_yield].values
-            max_tvl = self.dict[key]["TVL"][self.dict[key]["Yield"] == max_yield].values
+            max_exchange = self.dict[key]["Exchange"] \
+                           [self.dict[key]["Yield"] == max_yield].values
+            max_tvl = self.dict[key]["TVL"] \
+                      [self.dict[key]["Yield"] == max_yield].values
             # Ensure that value exists to avoid "list index out of range error" 
             if (len(max_exchange) > 0):
                 max_exchange = max_exchange[0]
@@ -41,16 +43,22 @@ class table:
                 max_exchange = ""
                 max_tvl = ""
             min_cost = self.dict[key]["Cost to Borrow"].min()
-            min_exchange = self.dict[key]["Exchange"][self.dict[key]["Cost to Borrow"] == min_cost].values
-            min_tvl = self.dict[key]["TVL"][self.dict[key]["Cost to Borrow"] == min_cost].values
-            # Ensure that value exists to avoid "list index out of range error"
+            min_exchange = self.dict[key]["Exchange"] \
+                           [self.dict[key]["Cost to Borrow"] == min_cost] \
+                           .values
+            min_tvl = self.dict[key]["TVL"][self.dict[key] \
+                      ["Cost to Borrow"] == min_cost].values
+            # Ensure that value exists to avoid 
+            # "list index out of range error"
             if (len(min_exchange) > 0):
                 min_exchange = min_exchange[0]
                 min_tvl = format_TVL(min_tvl[0])
             else:
                 min_exchange = ""
                 min_tvl = ""
-            cols = ["Max Yield", "Max Yield Exchange", "Max Yield TVL", "Min Cost", "Min Cost Exchange", "Min Cost TVL", "Spread", "Color"]
+            cols = ["Max Yield", "Max Yield Exchange", "Max Yield TVL", 
+                    "Min Cost", "Min Cost Exchange", "Min Cost TVL", 
+                    "Spread", "Color"]
             spread = "-"
             color = ""
             if (np.isnan(max_yield) == False):
@@ -69,7 +77,9 @@ class table:
                 min_cost = "{:.2f}%".format(min_cost)
             else: 
                 min_cost = "-"
-            data = [max_yield, max_exchange, max_tvl, min_cost, min_exchange, min_tvl, spread, color]
+            data = [max_yield, max_exchange, max_tvl, 
+                    min_cost, min_exchange, min_tvl, 
+                    spread, color]
             spreads[key] = pd.DataFrame([data], columns=cols)
         return spreads
 
